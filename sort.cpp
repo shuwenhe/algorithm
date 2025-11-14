a98a27f0316fd8acf5c3c8254d431ebd20eb4722
shuwen <1201220707@pku.edu.cn>
2025-11-15 00:11:38 +0800

    feat:sort pair

--- sort.cpp
+++ sort.cpp
@@ -7,5 +7,17 @@ int main(){
 	std::sort(a.begin(),a.end());
 
 	for(int x : a) std::cout<<x<<" ";
+
+	std::string s = "dbca";
+	std::sort(s.begin(),s.end());
+	std::cout<<s;
+
+	std::vector<std::pair<int,int>> v = {{3,4},{1,2},{3,1}};
+	std::sort(v.begin(),v.end());
+	for(auto &p : v){
+		std::cout<<"("<<p.first<<","<<p.second<<")";
+	}
+	std::cout<<std::endl;
+
 	return 0;
 }
