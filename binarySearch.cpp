2ef7211922f530def83260118289c65d9b121da0
Richard <richard@mit.edu.cn>
2025-11-29 17:26:04 +0800

    feat:binary search

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -2,6 +2,25 @@
 
 using namespace std;
 
-int main(){
-	cout<<"hello"<<endl;
+int binarySearch(vector<int> &num,int target){
+	int middle;
+	int left,right;
+	while(left<=right){
+		middle = left+(right-left)/2;
+		if(num.left<num.middle){
+			left = middle+1;
+		}else{
+			right = middle-1;
+		}
+		return middle;
+	}
+	return -1;
 }
+
+int main(){
+	vector<int> num = {1,3,5,7,9,11,13,15};
+	int target;
+	int result = binarySearch(num,target);
+	return 0;
+}
+
